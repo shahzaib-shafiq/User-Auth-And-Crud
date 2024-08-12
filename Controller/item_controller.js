b2f80c9728d75c3bf82086e1af0bfb49698aed55
Shahzaib Shafiq <shahzaib.shafiq@thecherrybyte.com>
2024-08-12 16:27:24 +0500

    Fetched All Items Sucessfully

--- Controller/item_controller.js
+++ Controller/item_controller.js
@@ -17,9 +17,41 @@ exports.addItem = async (req, res) => {
       });
     }
   } catch (error) {
-    console.error("Error in Item Details", error);
+    console.error("Error in Adding Item", error);
     res.status(500).json({
       message: error.message,
     });
   }
+};
+
+exports.getItemsbyId = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.getAllItems = async (req, res) => {
+  try {
+    const item = await Item.findAll();
+    if (item) {
+      res.status(201).json({
+        message: "Item Fetched successfully",
+        item,
+      });
+    }
+  } catch (error) {
+    console.error("Error in Fetching Items", error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteItem = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+exports.updateItem = async (req, res) => {
+  try {
+  } catch (error) {}
 };
--- Routes/item_route.js
+++ Routes/item_route.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 app.use(express.json());
-const { addItem } = require("../Controller/item_controller");
+const { addItem, getAllItems } = require("../Controller/item_controller");
 
 router.post("/additem", addItem);
+router.get("/getallitems", getAllItems);
 
 module.exports = router;
