b9c19ff7e09c595e900ab009f63dfde891e5f511
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-08-19 18:02:45 +0500

    Refactored Auth Code

--- src/middleware/Google_Auth.js
+++ src/middleware/Google_Auth.js
@@ -55,29 +55,6 @@ router.get("/auth/google/url", (req, res) => {
   return res.json({ url: googleAuthURL });
 });
 
-// const verifyToken = async (token) => {
-//   try {
-//     const client = new OAuth2Client(GOOGLE_CLIENT_ID);
-
-//     const ticket = await client.verifyIdToken({
-//       idToken: token,
-//       audience: GOOGLE_CLIENT_ID,
-//     });
-
-//     const returnedTicket = ticket;
-//     console.log(returnedTicket);
-//   } catch (error) {
-//     return { status: 500, data: error };
-//   }
-// };
-
-// router.get("/auth/verify/token", (req, res) => {
-//   const ver = verifyToken();
-
-//   return res.json({ url: ver });
-// });
-
-//////////////
 router.get("/auth/google/callback", async (req, res) => {
   const code = req.query.code;
 
@@ -120,56 +97,12 @@ router.get("/auth/google/callback", async (req, res) => {
       userInfoResponse
     );
 
-    // Return user info or handle it as needd
     return res.status(200).json({ message: "qwerty----------------------" });
   } catch (error) {
     console.error("Error during Google authentication:", error);
     res.status(500).json({ error: "Authentication failed" });
   }
 });
-
-// router.get("/auth/google/callback", async (req, res) => {
-//   const code = req.query.code;
-//   console.log("============================", code);
-//   if (!code) {
-//     return res.status(400).json({ error: "Authorization code is missing" });
-//   }
-
-//   try {
-//     // Exchange the authorization code for tokens
-//     const { tokens } = await client.getToken(code);
-
-//     const { access_token, id_token } = tokens;
-
-//     // Decode the ID token
-//     let userInfo;
-//     if (id_token) {
-//       const decodedIdToken = jwt.decode(id_token);
-//       console.log("==============================", decodedIdToken);
-//       userInfo = decodedIdToken;
-//       console.log("==============================", userInfo);
-//     }
-
-//     // Use the access token to get user info (optional)
-//     const userInfoResponse = await axios.get(
-//       "https://www.googleapis.com/oauth2/v2/userinfo",
-//       {
-//         headers: {
-//           Authorization: `Bearer ${access_token}`,
-//         },
-//       }
-//     );
-
-//     // Return user info or handle it as needed
-//     res.json({
-//       userInfo: userInfo,
-//       googleUserInfo: userInfoResponse.data,
-//     });
-//   } catch (error) {
-//     console.error("Error during Google authentication:", error);
-//     res.status(500).json({ error: "Authentication failed" });
-//   }
-// });
 
 async function getTokens({ code, clientId, clientSecret, redirectUri }) {
   const url = "https://oauth2.googleapis.com/token";
@@ -194,48 +127,6 @@ async function getTokens({ code, clientId, clientSecret, redirectUri }) {
   }
 }
 
-// Getting the user from Google with the code
-// router.get("/auth/google", async (req, res) => {
-//   const code = req.query.code;
-
-//   if (!code) {
-//     return res.status(400).send("Missing authorization code");
-//   }
-
-//   try {
-//     const { id_token, access_token } = await getTokens({
-//       code,
-//       clientId: GOOGLE_CLIENT_ID,
-//       clientSecret: GOOGLE_CLIENT_SECRET,
-//       redirectUri: `${SERVER_ROOT_URI}/${redirectURI}`,
-//     });
-
-//     // Fetch the user's profile with the access token
-//     const googleUser = await axios.get(
-//       `https://www.googleapis.com/oauth2/v1/userinfo?alt=json`,
-//       {
-//         headers: {
-//           Authorization: `Bearer ${access_token}`,
-//         },
-//       }
-//     );
-
-//     const token = jwt.sign(googleUser.data, JWT_SECRET);
-
-//     res.cookie(COOKIE_NAME, token, {
-//       maxAge: 900000,
-//       httpOnly: true,
-//       secure: false,
-//     });
-
-//     res.redirect(UI_ROOT_URI);
-//   } catch (error) {
-//     console.error(`Failed to process authentication`);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// Getting the current user
 router.get("/auth/me", (req, res) => {
   console.log("get me");
   try {
